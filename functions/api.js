1ee4b0138113ee5309bbfe81ad407ac74e499e41
ELWOIC <aswanthkrishnak822@gmail.com>
2026-02-24 21:39:09 +0530

    Create api.js

---
+++ functions/api.js
@@ -0,0 +1,19 @@
+export async function onRequest() {
+  try {
+    const response = await fetch(
+      "https://api.airgradient.com/public/api/v1/locations/measures/current?token=1cbd5f6c-1a7d-4c95-8c5f-5431bb412a8d"
+    );
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+
+  } catch (error) {
+    return new Response("Error fetching data", { status: 500 });
+  }
+}
