7f68dc64945250c1c352d4bc1f41c137881c6998
ELWOIC <aswanthkrishnak822@gmail.com>
2026-03-07 19:57:09 +0530

    Update imd-marquee.js

--- imd-marquee.js
+++ imd-marquee.js
@@ -2,12 +2,12 @@
 
 // Preset alert data
 const alerts = {
-  "2026-03-01": { text: "IMD Alert For Malappuram District: y" },
-  "2026-03-02": { text: "IMD Alert For Malappuram District: g" },
-  "2026-03-03": { text: "IMD Alert For Malappuram District: g" },
-  "2026-03-04": { text: "IMD Alert For Malappuram District: g" },
-  "2026-03-05": { text: "IMD Alert For Malappuram District: g" },
-  "2026-03-06": { text: "IMD Alert For Malappuram District: g" }
+  "2026-03-07": { text: "IMD Alert For Malappuram District: g" },
+  "2026-03-08": { text: "IMD Alert For Malappuram District: y" },
+  "2026-03-09": { text: "IMD Alert For Malappuram District: y" },
+  "2026-03-10": { text: "IMD Alert For Malappuram District: g" },
+  "2026-03-11": { text: "IMD Alert For Malappuram District: g" },
+  "2026-03-12": { text: "IMD Alert For Malappuram District: g" }
 };
 
 // Last updated time (manual)
